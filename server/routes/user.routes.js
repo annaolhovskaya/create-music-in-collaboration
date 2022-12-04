c18420c9f3c7bc43f8569473330c25ebcc09f254
Anna Olhovskaya <olhovskaya88@gmail.com>
2022-12-04 19:56:03 +0300

    dev server user route

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -1,4 +1,38 @@
 const express = require('express');
+const User = require('../models/User');
+const auth = require('../middleware/auth.middleware');
 const router = express.Router();
+
+router.patch('/:userId', auth, async (req, res) => {
+  try {
+    const { userId } = req.params;
+
+    if (userId === req.user._id) {
+      const updatedUser = await User.findByIdAndUpdate(userId, req.body, {
+        new: true,
+      });
+      res.status(200).send(updatedUser);
+    } else {
+      res.status(401).json({
+        message: 'Unauthorized',
+      });
+    }
+  } catch (error) {
+    res.status(500).json({
+      message: 'На сервере произошла ошибка. Попробуйте позже.',
+    });
+  }
+});
+
+router.get('/', auth, async (req, res) => {
+  try {
+    const list = await User.find();
+    res.status(200).send(list);
+  } catch (error) {
+    res.status(500).json({
+      message: 'На сервере произошла ошибка. Попробуйте позже.',
+    });
+  }
+});
 
 module.exports = router;
